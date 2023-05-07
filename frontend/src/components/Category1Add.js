1a608a32ab7ad1a39e49475baf59e8c6bc5c8936
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-05-07 08:38:13 +0530

    Added test statement

--- frontend/src/components/Category1Add.js
+++ frontend/src/components/Category1Add.js
@@ -1,54 +1,69 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default function Category1Add(){
+export default function Category1Add() {
+  const [name, setName] = useState("");
+  const [test, setTest] = useState({});
 
-    const [name, setName] = useState("");
+  function sendData(e) {
+    e.preventDefault();
 
-    function sendData(e){
-        e.preventDefault();
+    const newCategory = { name };
 
-        const newCategory = {name}
+    axios
+      .post("http://localhost:8070/category1/add", newCategory)
+      .then(() => {
+        alert("Category Added !");
+        window.location.replace(
+          "https://6428f31db9346d21afc59301--stunning-pegasus-128f89.netlify.app/categories"
+        );
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
 
-        axios.post("http://localhost:8070/category1/add", newCategory).then(()=>{
-            alert("Category Added !");
-            window.location.replace("https://6428f31db9346d21afc59301--stunning-pegasus-128f89.netlify.app/categories");
-        }).catch((err)=>{
-            alert(err);
-        })
-
-    }
-
-    return(
-        <div className="container">
-            <br></br>
-            <div className="row">
-                <div className="col">
-                <h1>Add a New Category</h1>
-                </div>
-            </div>
-            <hr></hr>
-            <br></br>
-            <form onSubmit={sendData}>
-                <div className="row">
-                    <div className="col">
-                        <label for="name" class="form-label">Category Name</label>
-                        <input type="text" class="form-control" id="name" placeholder="Eg: Men" required onChange={(e)=>{
-                            setName(e.target.value);
-                        }}/>
-                    </div>
-                    <div className="col">
-
-                    </div>
-                </div>
-                <br></br>
-                <br></br>
-                <a href="/categories" className="btn btn-secondary">Back <i class="fa fa-reply"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <button type="submit" className="btn btn-success">Submit <i class="fa fa-check"></i></button>
-                <br></br>
-                <br></br>
-            </form>
+  return (
+    <div className="container">
+      <br></br>
+      <div className="row">
+        <div className="col">
+          <h1>Add a New Category</h1>
         </div>
-    )
+      </div>
+      <hr></hr>
+      <br></br>
+      <form onSubmit={sendData}>
+        <div className="row">
+          <div className="col">
+            <label for="name" class="form-label">
+              Category Name
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              placeholder="Eg: Men"
+              required
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+          </div>
+          <div className="col"></div>
+        </div>
+        <br></br>
+        <br></br>
+        <a href="/categories" className="btn btn-secondary">
+          Back <i class="fa fa-reply"></i>
+        </a>
+        &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+        <button type="submit" className="btn btn-success">
+          Submit <i class="fa fa-check"></i>
+        </button>
+        <br></br>
+        <br></br>
+      </form>
+    </div>
+  );
 }
